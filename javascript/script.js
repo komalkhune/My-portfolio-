501009b0cd8e9d00c240f020a79172ccb2eeafec
Komal Khune <144751813+komalkhune@users.noreply.github.com>
2026-06-01 11:53:28 +0530

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,45 @@
+function toggleMenu(){
+    document.querySelector(".mview")
+    .classList.toggle("show");
+}
+   
+   
+   var typed = new Typed("#typing", {
+    strings: ["React Developer", "Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true
+  });
+
+
+   AOS.init();
+
+
+
+
+
+   emailjs.init("hlDC36PxZOh0TiyLb");
+
+document
+.getElementById("contactForm")
+.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    emailjs.send("service_w9q7lre", "template_wbj35os", {
+        from_name: document.getElementById("name").value,
+        from_email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    })
+
+    .then((success)=>{
+        alert("Message Sent Successfully!");
+
+        document.getElementById("contactForm").reset();
+    }).catch((error)=>{
+          console.log(error);
+        alert("Failed to send message");
+    })
+
+});
